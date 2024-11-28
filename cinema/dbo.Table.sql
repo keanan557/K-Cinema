495f84c6ef7961f46ef93cca29e8aeb8b4106dab
Keanan_Oliver <151459049+keanan557@users.noreply.github.com>
2024-11-28 11:57:46 +0200

    Add files via upload

---
+++ cinema/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Cinema] (
+    [Id]          INT           NOT NULL,
+    [Movies]      VARCHAR (50)  NULL,
+    [Description] VARCHAR (50)  NULL,
+    [Directors]   VARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
